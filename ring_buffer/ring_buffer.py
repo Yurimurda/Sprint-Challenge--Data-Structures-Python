445cbcc509994f32c53ab2dcd7d3f16d8929f0d9
Ian <lolsugotpwnd@yahoo.com>
2020-06-01 22:53:02 -0500

    ring_buffer complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,10 +2,19 @@ class RingBuffer:
     def __init__(self, capacity):
         self.data = []
         self.capacity = capacity
-        
+        self.current = 0
 
     def append(self, item):
-        self.data.append(item)
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+        else:
+            self.data[self.current] = item 
+            self.current += 1  
+
+            if len(self.data) == self.current:
+                self.current = 0
+        
+
         
 
     def get(self):
